a52cd7d656a7a218d7da0f9bab78e5bfa38ed776
Alirezaw <Alirezawmoradi@gmail.com>
2024-05-05 12:42:47 +0330

    code refactored

--- src/hooks/usePersianNumber.tsx
+++ src/hooks/usePersianNumber.tsx
@@ -2,9 +2,8 @@ import {useMemo} from "react";
 
 export const usePersianNumber = (number: number) => {
 
-    const PersianNumber = useMemo(() => {
+    return useMemo(() => {
         return new Intl.NumberFormat('fa-IR', {style: "decimal"}).format(number).replace(/٬/g, "");
     }, [number]);
 
-    return PersianNumber;
 }
